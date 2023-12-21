19bc6ff7297970bf0ad5c7563b033bb8fbbd508f
suddiyo <suddiyo@naver.com>
2023-12-21 23:24:33 +0900

    [Feat/#5] Kafka Consumer, Producer 설정

---
+++ kafka/src/main/java/com/asg/chat/config/WebSocketConfig.java
@@ -0,0 +1,2 @@
+package com.asg.chat.config;public class WebSocketConfig {
+}
--- kafka/src/main/java/com/asg/chat/kafka/ListenerConfiguration.java
+++ kafka/src/main/java/com/asg/chat/kafka/ListenerConfiguration.java
@@ -1,7 +1,9 @@
 package com.asg.chat.kafka;
 
 import com.asg.chat.domain.Message;
+import com.google.common.collect.ImmutableMap;
 import java.util.Map;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,9 +13,10 @@ import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
 import org.springframework.kafka.support.serializer.JsonDeserializer;
 
+
 @EnableKafka
 @Configuration
-public class KafkaConsumerConfig {
+public class ListenerConfiguration {
 
     // KafkaListener 컨테이너 팩토리를 생성하는 Bean 메서드
     @Bean
@@ -33,11 +36,11 @@ public class KafkaConsumerConfig {
         // Kafka Consumer 구성을 위한 설정값들을 설정 -> 변하지 않는 값이므로 ImmutableMap을 이용하여 설정
         Map<String, Object> consumerConfigurations =
                 ImmutableMap.<String, Object>builder()
-                        .put(KafkaConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092")
-                        .put(KafkaConsumerConfig.GROUP_ID_CONFIG, "adopt")
-                        .put(KafkaConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class)
-                        .put(KafkaConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, deserializer)
-                        .put(KafkaConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "latest")
+                        .put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092")
+                        .put(ConsumerConfig.GROUP_ID_CONFIG, "adopt")
+                        .put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class)
+                        .put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, deserializer)
+                        .put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "latest")
                         .build();
 
         return new DefaultKafkaConsumerFactory<>(consumerConfigurations, new StringDeserializer(), deserializer);
